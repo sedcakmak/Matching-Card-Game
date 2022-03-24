22d9e071565da1d14553c53a9e7e27122bfdcbc2
Sedef Cakmak <cakmaksedef@gmail.com>
2022-03-24 17:27:19 +0300

    implemented the game mechanics

--- index.js
+++ index.js
@@ -1,59 +1,78 @@
-// window.onload = chooseCards2;
-
-const cards = [
-  "./images/Card 1.png",
-  "./images/Card 2.png",
-  "./images/Card 3.png",
-  "./images/Card 4.png",
-  "./images/Card 5.png",
-  "./images/Card 6.png",
-  "./images/Card 7.png",
-  "./images/Card 8.png",
-  "./images/Card 9.png",
-  "./images/Card 10.png",
-];
-
-function chooseCrds() {
-  var randomNum = Math.floor(Math.random() * cards.length);
-  document.getElementById("firstRow").src = cards[randomNum];
-  document.getElementById("secondRow").src = cards[randomNum];
-  document.getElementById("thirdRow").src = cards[randomNum];
-  document.getElementById("fourthRow").src = cards[randomNum];
-  document.getElementById("fifthRow").src = cards[randomNum];
-}
-
-function choosedCards2() {
-  var cardscopy = cards.slice(); //getting new copy of initial input
-  function getRandomImg() {
-    return cardscopy.splice(
-      [Math.floor(Math.random() * cardscopy.length)],
-      (1)[0]
-    ); //calculating a new random image and removing it from copy to exclude duplicates
-  }
-
-  document.getElementById("firstRow").src = "images/" + getRandomImg();
-  document.getElementById("secondRow").src = "images/" + getRandomImg();
-  document.getElementById("thirdRow").src = "images/" + getRandomImg();
-}
-
 const buttonElement = document.getElementById("intro-button");
 const backgroundElement = document.getElementById("background-intro");
 const wrapperElement = document.getElementById("wrapper-cards");
+const bgElement = document.getElementById("background-game");
 
 buttonElement.addEventListener("click", hideIntro);
 
 function hideIntro() {
   backgroundElement.style.opacity = "0";
   wrapperElement.style.opacity = "1";
+  bgElement.style.opacity = "1";
   setTimeout(function () {
     backgroundElement.style.display = "none";
   }, 2000);
   setTimeout(function () {
     wrapperElement.style.display = "block";
-    console.log("now");
-  }, 2000);
+  }, 4000);
 }
 
-// buttonElement.ontransitionend = () => {
-//   alert("Transition ended");
-// };
+const cardElement = document.querySelectorAll(".memory-card");
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  lockBoard = true;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  // lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1500);
+}
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cardElement.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 20);
+    card.style.order = randomPosition;
+  });
+})();
+
+cardElement.forEach((card) => card.addEventListener("click", flipCard));
