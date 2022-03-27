54de178f490a885626671ede1eb47226044b5e7f
Sedef Cakmak <cakmaksedef@gmail.com>
2022-03-27 19:13:34 +0300

    implemented howler.js for audio

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 // window.onload = function () {
 //   document.getElementById("audio_intro").play();
 // };
+
+var music = {
+  intro: new Howl({
+    src: ["./images/intro4.mp3"],
+    loop: true,
+    autostart: false,
+  }),
+};
+
+var sfx = {
+  howling: new Howl({
+    src: ["./images/howling.wav"],
+  }),
+  boost: new Howl({
+    src: ["https://assets.codepen.io/21542/howler-sfx-levelup.mp3"],
+    loop: false,
+    onend: function () {
+      console.log("Done playing sfx!");
+    },
+  }),
+};
+
+// var sound = new Howl({
+//   src: ["./images/intro4.mp3"],
+//   loop: true,
+// });
+
 const buttonElement = document.getElementById("intro-button");
 const buttonElement2 = document.getElementById("intro-button2");
 const backgroundElement = document.getElementById("background-intro");
@@ -8,23 +35,32 @@ const wrapperElement = document.getElementById("wrapper-cards");
 const backtoIntroButton = document.getElementById("back_to_intro");
 const bgElement = document.getElementById("background-game");
 // const audioElement = document.getElementById("howling");
-// let audio_howling = new Audio("./images/howling.wav");
+//let audio_howling = new Audio("./images/howling.wav");
 // let audio_intro = new Audio("./images/intro.wav");
+const muteButton = document.getElementById("mute_sounds");
 
 backtoIntroButton.addEventListener("click", goBackToIntro);
 buttonElement.addEventListener("click", hideIntro);
 buttonElement2.addEventListener("click", playIntro);
+// muteButton.addEventListener("click", mutePage);
 
- function playIntro() {
-
-   document.getElementById("audio_intro").play();
- }
+function playIntro() {
+  if (!music.intro.playing()) {
+    music.intro.play();
+  }
+}
 
 function hideIntro() {
   backgroundElement.style.opacity = "0";
   wrapperElement.style.opacity = "1";
   bgElement.style.opacity = "1";
-  // audio_howling.play();
+  sfx.howling.play();
+
+  var fadeouttime = 1500;
+  setTimeout(function () {
+    music.intro.fade(1, 0, fadeouttime);
+  }, fadeouttime);
+
   setTimeout(function () {
     backgroundElement.style.display = "none";
   }, 2000);
@@ -37,20 +73,24 @@ function goBackToIntro() {
   location.reload();
 }
 
+function mutePage() {
+  console.log("mutePage working");
+  return music.intro.playing() ? music.intro.pause() : playIntro();
+}
+
 const cardElement = document.querySelectorAll(".memory-card");
 const testEle = document.getElementById("restart");
-const movesEle = document.querySelector(".moves")
+const movesEle = document.querySelector(".moves");
 
 let moves = 0;
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
-
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-  this.classList.remove("flip_reset")
+  this.classList.remove("flip_reset");
   this.classList.add("flip");
 
   if (!hasFlippedCard) {
@@ -58,7 +98,7 @@ function flipCard() {
     firstCard = this;
     return;
   }
-console.log("flipCard working")
+  console.log("flipCard working");
   secondCard = this;
   lockBoard = true;
 
@@ -71,7 +111,6 @@ function checkForMatch() {
   isMatch ? disableCards() : unflipCards();
   moves++;
   movesEle.innerHTML = `${moves} moves`;
-
 }
 
 function disableCards() {
@@ -79,8 +118,7 @@ function disableCards() {
   secondCard.removeEventListener("click", flipCard);
   firstCard.classList.add("cardDisabled");
   secondCard.classList.add("cardDisabled");
-  console.log("disableCards working")
-
+  console.log("disableCards working");
 
   resetBoard();
 }
@@ -113,7 +151,7 @@ function shuffle() {
 // })();
 
 function flipCards() {
-  console.log("workingggg")
+  console.log("workingggg");
 }
 
 function startOver() {
@@ -121,14 +159,14 @@ function startOver() {
   // cardElement.forEach(card => card.classList.remove("active"));
   movesEle.innerHTML = `0 moves`;
   moves = 0;
-  cardElement.forEach(card => {
+  cardElement.forEach((card) => {
     if (card.classList.contains("cardDisabled")) {
       // card.style.transform ="rotate(7deg)";
       card.classList.add("flip_reset");
       card.classList.remove("cardDisabled");
-     };
-     card.addEventListener("click", flipCard)
-  })
+    }
+    card.addEventListener("click", flipCard);
+  });
   hasFlippedCard = false;
   lockBoard = false;
   resetBoard();
