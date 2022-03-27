eee4223ba2a14e29079f3eb0d5981f9f1c0c4a9a
Sedef Cakmak <cakmaksedef@gmail.com>
2022-03-27 17:09:29 +0300

    implemented restart game and back to the forest button

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const buttonElement = document.getElementById("intro-button");
 const buttonElement2 = document.getElementById("intro-button2");
 const backgroundElement = document.getElementById("background-intro");
 const wrapperElement = document.getElementById("wrapper-cards");
+const backtoIntroButton = document.getElementById("back_to_intro");
 const bgElement = document.getElementById("background-game");
 // const audioElement = document.getElementById("howling");
 // let audio_howling = new Audio("./images/howling.wav");
 // let audio_intro = new Audio("./images/intro.wav");
 
+backtoIntroButton.addEventListener("click", goBackToIntro);
 buttonElement.addEventListener("click", hideIntro);
 buttonElement2.addEventListener("click", playIntro);
 
  function playIntro() {
-   console.log("working");
+
    document.getElementById("audio_intro").play();
  }
 
@@ -31,19 +33,24 @@ function hideIntro() {
     shuffle();
   }, 4000);
 }
+function goBackToIntro() {
+  location.reload();
+}
 
 const cardElement = document.querySelectorAll(".memory-card");
-const testEle = document.getElementById("testing");
+const testEle = document.getElementById("restart");
+const movesEle = document.querySelector(".moves")
 
 let moves = 0;
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-
+  this.classList.remove("flip_reset")
   this.classList.add("flip");
 
   if (!hasFlippedCard) {
@@ -51,7 +58,7 @@ function flipCard() {
     firstCard = this;
     return;
   }
-
+console.log("flipCard working")
   secondCard = this;
   lockBoard = true;
 
@@ -62,11 +69,18 @@ function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
   isMatch ? disableCards() : unflipCards();
+  moves++;
+  movesEle.innerHTML = `${moves} moves`;
+
 }
 
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+  firstCard.classList.add("cardDisabled");
+  secondCard.classList.add("cardDisabled");
+  console.log("disableCards working")
+
 
   resetBoard();
 }
@@ -77,7 +91,7 @@ function unflipCards() {
     secondCard.classList.remove("flip");
 
     resetBoard();
-  }, 1500);
+  }, 1000);
 }
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
@@ -98,10 +112,30 @@ function shuffle() {
 //   });
 // })();
 
-function startOver() {
-  shuffle();
-  console.log("workingg");
+function flipCards() {
+  console.log("workingggg")
 }
 
+function startOver() {
+  shuffle();
+  // cardElement.forEach(card => card.classList.remove("active"));
+  movesEle.innerHTML = `0 moves`;
+  moves = 0;
+  cardElement.forEach(card => {
+    if (card.classList.contains("cardDisabled")) {
+      // card.style.transform ="rotate(7deg)";
+      card.classList.add("flip_reset");
+      card.classList.remove("cardDisabled");
+     };
+     card.addEventListener("click", flipCard)
+  })
+  hasFlippedCard = false;
+  lockBoard = false;
+  resetBoard();
+}
+
+// Back to forest:  card.classList.add("flip")
+//
+// console.log(card.getAttribute("data-framework"))
 cardElement.forEach((card) => card.addEventListener("click", flipCard));
 testEle.addEventListener("click", startOver);
